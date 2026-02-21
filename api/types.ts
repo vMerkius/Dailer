bd3b20c500ea8b717e736ab32a9e08a90400d114
Mateusz Rojek <merkius@wp.pl>
2026-02-21 23:11:16 +0100

    feat: add ILoginResponse type to enhance login response structure

--- api/types.ts
+++ api/types.ts
@@ -2,3 +2,11 @@ export type IUserCredentials = {
   username: string;
   password: string;
 };
+
+export type ILoginResponse = {
+  message: string;
+  statusCode: number;
+  accessToken: string;
+  refreshToken: string;
+  expiresIn: number;
+};
